67fa61f77ee8a9aa46fb550490ca9d49d6505345
vacant_head <suzuki.joichiro@gmail.com>
2024-03-12 17:36:46 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <Arduino.h>
+#include <math.h>
+#include <Wire.h>
+#include <PS4Controller.h>
+#include <esp_bt_defs.h>
+#include <esp_bt_main.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
