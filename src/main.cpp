4447126bfdd9d3ba72c89ec26f5b3fe68ccff2b0
vacant_head <suzuki.joichiro@gmail.com>
2024-04-07 00:26:20 +0900

    動いた

--- src/main.cpp
+++ src/main.cpp
@@ -14,39 +14,48 @@ int control_count = 0;
 int interval = 0;
 int preinterval = 0;
 
-// モーター
-const int top_motor_In1 = 25;
-const int top_motor_In2 = 26;
-const int bottom_motor_In1 = 27;
-const int bottom_motor_In2 = 32;
+// フィードバックゲイン
+float steer_gainP = 160.0;
+float steer_gainI = 0.0;
+float steer_gainD = 0.0;
+
+// ledcWrite()
+int ledcfreq = 20000;   // Hz
+int ledcResolution = 8; // bit
+
+// モーターピン･チャンネル
+const int top_motor_In1 = 32;
+const int top_motor_In2 = 25;
+const int bottom_motor_In1 = 26;
+const int bottom_motor_In2 = 27;
 const int ch_top1 = 0;
 const int ch_top2 = 1;
 const int ch_bottom1 = 2;
 const int ch_bottom2 = 3;
 
-// 磁気エンコーダ
+// 磁気エンコーダ I2C
 const int steer_encoder_SCL = 22;
 const int steer_encoder_SDA = 21;
 
 // ローターリーエンコーダ
-const int top_encoder_A = 16;
-const int top_encoder_B = 17;
-const int bottom_encoder_A = 18;
-const int bottom_encoder_B = 19;
+const int top_encoder_A = 18;
+const int top_encoder_B = 19;
+const int bottom_encoder_A = 16;
+const int bottom_encoder_B = 17;
 
-// ゲイン調整用ポテンショピン
+// ゲイン調整用ポテンショ
 const int pot1 = 34;
 const int pot2 = 35;
 const int pot3 = 36;
 
 // 状態表示LED
-const int statusLED1 = 13;
-const int statusLED2 = 14;
+const int statusLED1 = 14;
+const int statusLED2 = 4;
 const int ch_led1 = 5;
 const int ch_led2 = 6;
 
 // スイッチ
-const int sw_pin=13;
+const int sw_pin = 13;
 
 // ps4コントローラーの指令値
 float command_X = 0.f;
@@ -55,12 +64,9 @@ float command_steer = 0.0; // rad
 float command_speed = 0.0;
 
 // ステア角フィードバック
-float steer_angle = 0.0;     // deg 角度
-float steer_pre_angle = 0.0; // deg前周期のステア角
-float steer_angle_ref = 0.0; // deg 目標値
-float steer_gainP = 0.0;
-float steer_gainI = 0.0;
-float steer_gainD = 0.0;
+float steer_angle = 0.0;     // 角度
+float steer_pre_angle = 0.0; // 前周期のステア角
+float steer_angle_ref = 0.0; // 目標値
 float err_steer = 0.0;
 float pre_err_steer = 0.0;
 float steer_calcP = 0.0;
@@ -78,27 +84,33 @@ void setup()
   Serial.begin(57600);
   PS4.begin("0C:B8:15:C1:3C:66");
   Wire.begin();
+  // エンコーダ
   qei_setup_x4(PCNT_UNIT_0, top_encoder_A, top_encoder_B);
   qei_setup_x4(PCNT_UNIT_1, bottom_encoder_A, bottom_encoder_B);
-  int ledcfreq = 20000;   // Hz
-  int ledcResolution = 8; // bit
+
+  // モーターの宣言
   ledcAttachPin(top_motor_In1, ch_top1);
   ledcAttachPin(top_motor_In2, ch_top2);
   ledcAttachPin(bottom_motor_In1, ch_bottom1);
-  ledcAttachPin(bottom_motor_In1, ch_bottom2);
+  ledcAttachPin(bottom_motor_In2, ch_bottom2);
   ledcSetup(ch_top1, ledcfreq, ledcResolution);
   ledcSetup(ch_top2, ledcfreq, ledcResolution);
   ledcSetup(ch_bottom1, ledcfreq, ledcResolution);
-  ledcSetup(ch_bottom1, ledcfreq, ledcResolution);
+  ledcSetup(ch_bottom2, ledcfreq, ledcResolution);
+
   // ゲイン調整用ポテンショ
   pinMode(pot1, INPUT);
   pinMode(pot2, INPUT);
   pinMode(pot3, INPUT);
+
   // 状態表示LED
   ledcAttachPin(statusLED1, ch_led1);
   ledcSetup(ch_led1, ledcfreq, ledcResolution);
-  // pinMode(statusLED1, OUTPUT);
-  // pinMode(statusLED2, OUTPUT);
+  ledcAttachPin(statusLED2, ch_led2);
+  ledcSetup(ch_led2, ledcfreq, ledcResolution);
+
+  // スイッチ
+  pinMode(sw_pin, INPUT);
 }
 
 void loop()
@@ -106,34 +118,49 @@ void loop()
   /*PS4コントローラー*/
   if (PS4.isConnected())
   {
-    // スティックの値を-1~1に変換
-    command_X = (float)PS4.LStickX() / 128.0f;
-    command_Y = (float)PS4.LStickY() / 128.0f;
-    // スティックの値を-1~1に変換、その後速度指令値の上限1になるようルート2でわる
-    // command_X = (float)PS4.LStickX() * 0.005524271728f; //(1/128)*(1/sqrt(2))
-    // command_Y = (float)PS4.LStickY() * 0.005524271728f;
+    // 手を離した状態でのスティックの値を完全にゼロに補正
+    // Lスティックの値を読み、-1~1に変換
+    if (PS4.LStickX() >= 10 || PS4.LStickX() <= -10)
+    {
+      command_X = (float)PS4.LStickX() / 128.0f;
+    }
+    else
+    {
+      command_X = 0;
+    }
+    if (PS4.LStickY() >= 10 || PS4.LStickY() <= -10)
+    {
+      command_Y = (float)PS4.LStickY() / 128.0f;
+    }
+    else
+    {
+      command_Y = 0;
+    }
+
     // ステア角指令値
     command_steer = (float)atan2(command_Y, command_X);
+    // 0~360度に変換
+    if (command_Y < 0)
+    {
+      command_steer += 2 * M_PI;
+    }
+
     // 速度指令値
     command_speed = sqrt(command_Y * command_Y + command_X * command_X);
+    if (command_speed >= 1.0)
+    {
+      command_speed = 1.0;
+    }
     // LED消灯
-    ledcWrite(statusLED1, 0);
+    ledcWrite(ch_led1, 0);
   }
   else
   {
     // LED点灯
-    ledcWrite(statusLED1, 255);
+    // ledcWrite(ch_led1, 255);
     // 指令値を0に
     command_steer = 0.0f;
     command_speed = 0.0f;
-  }
-
-  if (control_count % 50)
-  {
-    // 指令値ベクトル
-    Serial.printf("%f,%f/r/n", command_X, command_Y);
-    // ステア指令値と速度指令値
-    Serial.printf("%f,%f/r/n", command_steer, command_speed);
   }
 
   /*ステアエンコーダー*/
@@ -149,9 +176,9 @@ void loop()
     byte angle_l = Wire.read();
     unsigned int angle = (0x0F & angle_h) << 8 | angle_l;
     steer_angle = (float)angle * 2 * M_PI / 4096.f;
-    Serial.println(angle);
-    // Serial.print(angle,HEX);
-    // Serial.print(" ");
+    // Serial.println(steer_angle);
+    //  Serial.print(angle,HEX);
+    //  Serial.print(" ");
   }
   // delay(1);
 
@@ -168,48 +195,60 @@ void loop()
   //   Serial.println(state, BIN);
   // }
 
-  /* ステア角フィードバック*/
+  /* ステア角フィードバック */
+  // 目標値生成
+  //  command_steer = 1.0;
+  steer_angle_ref = command_steer - steer_angle;
+  // ステア角の最適化
+  if (steer_angle_ref <= -1.0 * M_PI)
+  {
+    steer_angle_ref = (2 * M_PI - steer_angle) + command_steer;
+  }
+  if (steer_angle_ref >= M_PI)
+  {
+    steer_angle_ref = -steer_angle - (2 * M_PI - command_steer);
+  }
+  err_steer = steer_angle_ref;
+
   // ゲイン調整
-  steer_gainP = 0.0f + (float)(analogRead(pot1) - 2048) * 0.00024414f * 0.0001f;
-  steer_gainI = 0.0f + (float)(analogRead(pot2) - 2048) * 0.00024414f * 0.0f;
-  steer_gainD = 0.0f + (float)(analogRead(pot3) - 2048) * 0.00024414f * 0.0f;
-  // ステア角度変化目標値
-  // steer_angle_ref = command_steer - steer_angle;
-  err_steer = command_steer - steer_angle;
-  // err_steer = steer_angle - steer_angle_ref;
-  // steer_vel=err_steer/(control_period*10.);
+  //steer_gainP = 110.0f + (float)(analogRead(pot1) - 2048) * 0.00024414f * 100.0f;
+  // steer_gainI = 0.0f + (float)(analogRead(pot2) - 2048) * 0.00024414f * 0.0f;
+  // steer_gainD = 0.0f + (float)(analogRead(pot3) - 2048) * 0.00024414f * 0.0f;
+
   // フィードバック項の計算
   steer_calcP = err_steer;
   steer_calcI = steer_calcI + (err_steer + pre_err_steer) * ((float)control_period * 1.0E-6f) * 0.5;
   steer_calcD = (err_steer - pre_err_steer) * control_freq;
-  steer_calcPID = steer_gainP * steer_calcP + steer_gainI * steer_calcI + steer_gainD * steer_calcD;
+  steer_calcPID = -1.0 * (steer_gainP * steer_calcP + steer_gainI * steer_calcI + steer_gainD * steer_calcD);
 
   pre_err_steer = err_steer;
   steer_pre_angle = steer_angle;
 
-  if (steer_calcPID > 1.0)
+  if (steer_calcPID > 255.0)
   {
-    steer_calcPID = 1.0;
+    steer_calcPID = 255;
   }
-  if (steer_calcPID < -1.0)
+  if (steer_calcPID < -255)
   {
-    steer_calcPID = -1.0;
+    steer_calcPID = -255;
   }
 
   // モーターの出力に変換
-  if (err_steer >= 0.0628)
+  if (err_steer >= 0.0628 * 20.0)
   {
     command_speed = 0.0;
   }
-  top_motor_duty_out = command_speed * 0.25 + steer_calcPID;
-  bottom_motor_duty_out = command_speed * 0.25 - steer_calcPID;
+  top_motor_duty_out = 255.0 * command_speed * 0.25 * 4.0 + steer_calcPID;
+  bottom_motor_duty_out = 255.0 * command_speed * 0.25 * 4.0 - steer_calcPID;
 
   if (control_count % 50)
   {
-    Serial.printf("%f,%f,%f/r/n", steer_gainP, steer_gainD, steer_gainD);
-    Serial.printf("%f/r/n", top_motor_duty_out, bottom_motor_duty_out);
-    Serial.printf("%f/r/n", err_steer);
-    Serial.printf("%f,%f/r/n", top_motor_duty_out, bottom_motor_duty_out);
+    // Serial.printf("%f,%f,%f\r\n", steer_gainP, steer_gainD, steer_gainD); // ゲイン
+    //  Serial.printf("%f,%f\r\n", top_motor_duty_out, bottom_motor_duty_out); // デューティ
+    //  Serial.printf("%f\r\n", err_steer); // 偏差
+    //  Serial.printf("%f,%f\r\n", command_X, command_Y); // 指令値ベクトル
+    // Serial.printf("%f,%f\r\n", command_steer, command_speed); // ステア指令値と速度指令値
+    //  Serial.println(steer_angle); // ステア角
   }
 
   if (top_motor_duty_out > 255)
@@ -229,11 +268,16 @@ void loop()
     bottom_motor_duty_out = -255;
   }
 
-  if(digitalRead(sw_pin)==0)
-  {
-    top_motor_duty_out=0.0f;
-    bottom_motor_duty_out=0.0f;
-  }
+  // if (digitalRead(sw_pin) == LOW)
+  // {
+  //   top_motor_duty_out = 0.0f;
+  //   bottom_motor_duty_out = 0.0f;
+  //   // ledcWrite(ch_led2, 255);
+  //   Serial.printf("off\n");
+  // }
+  // else{
+  //   ledcWrite(ch_led2,0);
+  // }
 
   if (top_motor_duty_out >= 0.0)
   {
